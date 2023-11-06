33ed393e6df71cf7e0955dd1095a4e6844f62761
zahralatif <zahralatif2107@gmail.com>
2023-11-06 22:04:13 +0400

    scroll bar design

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDPzCRBa-pYhhTX-PXeqy_kx8EVV00LYx4",
-  authDomain: "kaizen-edu-az.firebaseapp.com",
-  projectId: "kaizen-edu-az",
-  storageBucket: "kaizen-edu-az.appspot.com",
-  messagingSenderId: "955498457376",
-  appId: "1:955498457376:web:a05a5946b056ef25a94f45",
-  measurementId: "G-V3RK9EHTSK"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
 const navMenu = document.getElementById('nav-menu'),
     navToggle = document.getElementById('nav-toggle'),
     icon = document.querySelector('.bx-menu')
@@ -54,3 +32,26 @@ const scrollUp = () => {
 
 
 window.addEventListener('scroll', scrollUp)
+
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDPzCRBa-pYhhTX-PXeqy_kx8EVV00LYx4",
+//   authDomain: "kaizen-edu-az.firebaseapp.com",
+//   projectId: "kaizen-edu-az",
+//   storageBucket: "kaizen-edu-az.appspot.com",
+//   messagingSenderId: "955498457376",
+//   appId: "1:955498457376:web:a05a5946b056ef25a94f45",
+//   measurementId: "G-V3RK9EHTSK"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
