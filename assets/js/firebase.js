265a41ab9076a58cd5b4755c5c8278e68dafcf7a
zahralatif <zahralatif2107@gmail.com>
2023-11-13 21:58:38 +0400

    admin-nav

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,14 +1,12 @@
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
 
   const firebaseConfig = {
-    apiKey: "AIzaSyA9Sj40XQLvQd3WsRGEFpd-Dm-rj_MrqGs",
-    authDomain: "kaizen-ecbd1.firebaseapp.com",
-    projectId: "kaizen-ecbd1",
-    storageBucket: "kaizen-ecbd1.appspot.com",
-    messagingSenderId: "434429707201",
-    appId: "1:434429707201:web:e46e0af31b2fb7d2f66540",
-    measurementId: "G-2W4SRTSPD7"
+    apiKey: "AIzaSyAIY14jK41b8UpxsPYCyagWOSrOfOvn1_U",
+    authDomain: "kaizen-f5210.firebaseapp.com",
+    projectId: "kaizen-f5210",
+    storageBucket: "kaizen-f5210.appspot.com",
+    messagingSenderId: "976186795292",
+    appId: "1:976186795292:web:a9457985a47b7be7df46d9"
   };
 
   const app = initializeApp(firebaseConfig);
-
